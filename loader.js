2133681ad4fe7176a342e185dedb781bab8bc672
Harry James Hocker <harry_hocker@icloud.com>
2022-02-14 14:11:41 -0500

    Initial commit

---
+++ loader.js
@@ -0,0 +1,21 @@
+
+class loader {
+
+    channel_id = null
+    token = null
+    list_of_people = []
+
+    constructor() {
+        this.operations()
+        this.generate_list()
+    }
+
+    operations() {
+
+    }
+
+    generate_list() {
+        
+    }
+
+}
