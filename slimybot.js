fd0500f72403c7e0ad8b1675de9e7e619191b7e4
Harry James Hocker <harry_hocker@icloud.com>
2022-03-07 00:28:37 -0500

    Added ability to read .json files.

---
+++ slimybot.js
@@ -0,0 +1,16 @@
+// Require the necessary discord.js classes and files
+const { Client, Intents } = require('discord.js');
+const settings = require('./settings.json');
+const list_of_people = require('./discord_users.json');
+
+// Create a new bot instance
+const slimybot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// When the client is ready, run this code (only once)
+slimybot.once('ready', () => {
+    console.log('Ready!');
+});
+
+// Login to Discord with your client's token
+slimybot.login(settings.bot_token).then();
+
